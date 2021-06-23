48b8c1cc814d5297720240d20aa860550acb9fd7
Kaitaku Takeda <katakeda32@gmail.com>
2021-06-23 06:28:58 -0700

    Add firebase util

---
+++ src/utils/firebase.ts
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { EmailPasswordCredentials } from '../types/common';
+
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
+
+export const app: firebase.app.App = firebase.initializeApp(firebaseConfig);
+
+export const signup = async ({ email, password }: EmailPasswordCredentials): Promise<firebase.auth.UserCredential> => {
+  return await firebase.auth().createUserWithEmailAndPassword(email, password);
+}
+
+export const login = async ({ email, password }: EmailPasswordCredentials): Promise<firebase.auth.UserCredential> => {
+  return await firebase.auth().signInWithEmailAndPassword(email, password);
+}
+
+export const googleSignin = (): void => {
+  firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+}
